682ee29f6a2a4fb0e365b41f58d22b9b8199cc64
olawale-o <omoogunolawale@gmail.com>
2021-08-19 15:57:01 +0100

    Import BrowserRouter in index.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
-const Header = () => {
-  return (
-    <header className="Header">
-      <nav>
-        <div className="logo">Bookstore CMS</div>
-          <ul>
-            <li>
-              <a href="/#">Books</a>
-            </li>
-            <li>
-              <a href="/#">Categories</a>
-            </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="Header">
+    <nav>
+      <div className="logo">Bookstore CMS</div>
+      <ul>
+        <li>
+          <a href="/#">Books</a>
+        </li>
+        <li>
+          <a href="/#">Categories</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
