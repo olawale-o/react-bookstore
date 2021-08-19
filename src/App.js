03768648f29c6f033e3feede4c37ddff89f89e2b
olawale-o <omoogunolawale@gmail.com>
2021-08-19 16:08:33 +0100

    Import Header.js in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Books from './components/Books';
 import Categories from './components/Categories';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/books" component={Books} />
         <Route path="/categories" component={Categories} />
