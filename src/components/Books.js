db95de9f59f2bf726408b9ec768fb8f95942a7ad
olawale-o <omoogunolawale@gmail.com>
2021-08-19 17:29:51 +0100

    Import AddBook.js in Books.js file

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import booksArray from '../data/index';
 import SingleBook from './SingleBook';
+import AddBook from './AddBook';
 
 const Books = () => {
   const books = booksArray.map((book) => <SingleBook book={book} key={book.id} />);
   return (
     <div className="Books">
-      { books }
+      <div className="book-lists">
+        { books }
+      </div>
+      <hr />
+      <AddBook />
     </div>
   );
 };
