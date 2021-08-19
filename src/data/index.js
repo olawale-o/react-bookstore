3b32e60eae6189f16e31638291e37d5dceccf061
olawale-o <omoogunolawale@gmail.com>
2021-08-19 17:01:38 +0100

    Create booksArray

---
+++ src/data/index.js
@@ -0,0 +1,31 @@
+const booksArray = [
+  {
+    id: 1,
+    categories: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    completed: '64',
+    currentChapter: 'Chapter 17',
+    chapterTitle: '',
+  },
+  {
+    id: 2,
+    categories: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+    completed: '8',
+    currentChapter: 'Chapter 3',
+    chapterTitle: 'A Lesson Learned',
+  },
+  {
+    id: 3,
+    categories: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    completed: '0',
+    currentChapter: '',
+    chapterTitle: 'Introduction',
+  },
+];
+
+export default booksArray;
