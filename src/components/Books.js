5c2ae8d3f0091addeac1413693c29457b8dbe4ea
olawale-o <omoogunolawale@gmail.com>
2021-08-19 21:03:19 +0100

    Dispatch createBook action Books.js file

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import booksArray from '../data/index';
 import SingleBook from './SingleBook';
 import AddBook from './AddBook';
+import { createBook } from '../redux/books/books_creators';
 
 const Books = () => {
+  const dispatch = useDispatch();
   const books = booksArray.map((book) => <SingleBook book={book} key={book.id} />);
+
+  const addBookToStore = (book) => {
+    dispatch(createBook(book));
+  };
+
   return (
     <div className="Books">
       <div className="book-lists">
         { books }
       </div>
       <hr />
-      <AddBook />
+      <AddBook addBookToStore={addBookToStore} />
     </div>
   );
 };
