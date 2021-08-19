1aa9e35bc9a5a89c0e8e21e876d2e5c2b3fd1ac5
olawale-o <omoogunolawale@gmail.com>
2021-08-19 17:17:31 +0100

    Modify SingleBook.js file

--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -4,12 +4,9 @@ import PropType from 'prop-types';
 const SingleBook = ({ book }) => (
   <div className="Book">
     <div className="book-summary">
-      <span className="book-category">
-        Action
-        {book}
-      </span>
-      <h4 className="book-title">The Hunger Games</h4>
-      <a href="/#">Suzane Collins</a>
+      <span className="book-category">{book.categories}</span>
+      <h4 className="book-title">{book.categories.title}</h4>
+      <a href="/#">{book.author}</a>
       <ul className="book-modify">
         <li>
           <a href="/#">Comments</a>
@@ -25,13 +22,20 @@ const SingleBook = ({ book }) => (
     <div className="book-progress">
       <div className="book-progress__indicator"> </div>
       <div className="book-progress__level">
-        <div className="book-progress__level-percentage">64%</div>
+        <div className="book-progress__level-percentage">
+          {book.completed}
+          %
+        </div>
         <div className="book-progress__level-status">Completed</div>
       </div>
     </div>
     <div className="book-chapter">
       <p className="book-chapter__current">CURRENT CHAPTER</p>
-      <p className="book-chapter__number">Chapter 17</p>
+      <p className="book-chapter__number">
+        {book.currentChapter}
+        {book.currentChapter && book.chapterTitle && ':'}
+        {book.chapterTitle}
+      </p>
       <button type="button">UPDATE PROGRESS</button>
     </div>
   </div>
@@ -40,5 +44,13 @@ const SingleBook = ({ book }) => (
 export default SingleBook;
 
 SingleBook.propTypes = {
-  book: PropType.number.isRequired,
+  book: PropType.shape({
+    id: PropType.number.isRequired,
+    categories: PropType.string.isRequired,
+    title: PropType.string.isRequired,
+    author: PropType.string.isRequired,
+    completed: PropType.string.isRequired,
+    currentChapter: PropType.string,
+    chapterTitle: PropType.string,
+  }).isRequired,
 };
