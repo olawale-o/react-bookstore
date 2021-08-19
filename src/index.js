ef649ef8b635c11b59f9b5759ddcf8b8a0fc9c9b
olawale-o <omoogunolawale@gmail.com>
2021-08-19 19:57:00 +0100

    Import configureStore.js and Provider in index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/configureStore';
+
+const store = configureStore;
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
