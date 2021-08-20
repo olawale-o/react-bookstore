eabf6a33e5b26ad6eb99a0c93545a57be4316451
olawale-o <omoogunolawale@gmail.com>
2021-08-20 16:17:08 +0100

    Change paylod to payload

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,9 +4,9 @@ import { CREATE_BOOK, REMOVE_BOOK } from './books_action_types';
 export default function booksReducer(state = initialState, action) {
   switch (action.type) {
     case CREATE_BOOK:
-      return [...state, action];
+      return [...state, action.payload];
     case REMOVE_BOOK: {
-      const filteredBooks = state.booksReducer.filteredBooks(({ id }) => action.paylod !== id);
+      const filteredBooks = state.booksReducer.filteredBooks(({ id }) => action.payload !== id);
       return filteredBooks;
     }
     default:
