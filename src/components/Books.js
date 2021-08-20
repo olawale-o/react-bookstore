f687d5c0ee1022de87715b66136107f79b90382a
olawale-o <omoogunolawale@gmail.com>
2021-08-20 16:24:34 +0100

    Use useSelector from react-redux in Books.js file

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import booksArray from '../data/index';
+import { useDispatch, useSelector } from 'react-redux';
+import booksSelector from '../redux/books/booksSelector';
 import SingleBook from './SingleBook';
 import AddBook from './AddBook';
 import { createBook } from '../redux/books/books_creators';
 
 const Books = () => {
   const dispatch = useDispatch();
-
+  const booksArray = useSelector(booksSelector);
   const books = booksArray.map((book) => <SingleBook book={book} key={book.id} />);
 
   const addBookToStore = (book) => {
-    dispatch(createBook(book));
+    const newBook = {
+      ...book,
+      author: 'Author',
+      percentProgress: '0',
+      currentChapter: 'Introduction',
+      chapterTitle: '',
+    };
+    dispatch(createBook(newBook));
   };
 
   return (
