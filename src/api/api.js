1ab1539306a320d21d15b68bbce38ea38eb33210
olawale-o <omoogunolawale@gmail.com>
2021-08-21 04:14:01 +0100

    Create post function in api.js file

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,16 @@
-const get = async (URI) => {
+export const get = async (URI) => {
   const response = await fetch(URI);
   return response.json();
 };
 
-export default get;
+export const post = async (URI, data = {}) => {
+  const response = await fetch(URI, {
+    mode: 'cors',
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; Charset=UTF-8',
+    },
+  });
+  return response;
+};
