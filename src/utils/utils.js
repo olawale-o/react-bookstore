407c398e6dc31da9c06a040dc0798dfc2e1f4d72
olawale-o <omoogunolawale@gmail.com>
2021-08-21 04:21:24 +0100

    Create postBook function in utils.js file

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,17 @@
+import { post
+ } from "../api/api";
 const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/';
 
-const createApp = async () => {
+export const createApp = async () => {
   const END_POINT = 'apps/';
   const URI = `${BASE_URL}${END_POINT}`;
   const response = await post(URI);
   return response.text();
+};
+
+export const postBook = async (appId, data) => {
+  const END_POINT = `apps/${appId}/books/`;
+  const URI = `${BASE_URL}${END_POINT}`;
+  const response = await post(URI, data);
+  return response.json();
 };
