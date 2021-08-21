d9141de098324c19839990a6d890a78f2c66d177
olawale-o <omoogunolawale@gmail.com>
2021-08-21 15:07:23 +0100

    Modify api.js file

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 export const get = async (URI) => {
   const response = await fetch(URI);
-  return response.json();
+  return response;
 };
 
-export const post = async (URI, data = {}) => {
+export const post = async (URI, data = {}, method = 'POST') => {
   const response = await fetch(URI, {
     mode: 'cors',
-    method: 'POST',
+    method,
     body: JSON.stringify(data),
     headers: {
       'Content-type': 'application/json; Charset=UTF-8',
