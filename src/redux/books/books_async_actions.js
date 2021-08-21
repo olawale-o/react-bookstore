327f93e53e3e59e411fa7cd9b83d83fbdccd0024
olawale-o <omoogunolawale@gmail.com>
2021-08-21 15:25:47 +0100

    Create addBook function in books_async_actions.js file

--- src/redux/books/books_async_actions.js
+++ src/redux/books/books_async_actions.js
@@ -1,29 +1,42 @@
-import { createApp, fetchAllBooks } from '../../utils/utils';
-import { loadBooks, noBooks } from './books_creators';
+import { createApp, fetchAllBooks, postBook } from '../../utils/utils';
+import {
+  createBook, loadBooks, noBooks,
+} from './books_creators';
 import { getStorage, setStorage } from '../../storage/storage';
 
-export default async function getBooks(dispatch) {
-  let appId = getStorage();
-  try {
-    if (!appId) {
-      appId = await createApp();
-      setStorage(appId);
+export const getBooks = () => (
+  async function getBooks(dispatch) {
+    let appId = getStorage();
+    try {
+      if (!appId) {
+        appId = await createApp();
+        setStorage(appId);
+      }
+      const allBooks = await fetchAllBooks(appId);
+      const bookEntries = Object.entries(allBooks);
+      const savedBooks = bookEntries.map(([key, value]) => {
+        const [book] = value;
+        return {
+          item_id: key,
+          ...book,
+          author: 'Author',
+          percentProgress: '0',
+          currentChapter: 'Introduction',
+          chapterTitle: '',
+        };
+      });
+      dispatch(loadBooks(savedBooks));
+    } catch (error) {
+      dispatch(noBooks(error));
     }
-    const allBooks = await fetchAllBooks(appId);
-    const bookEntries = Object.entries(allBooks);
-    const savedBooks = bookEntries.map(([key, value]) => {
-      const [book] = value;
-      return {
-        item_id: key,
-        ...book,
-        author: 'Author',
-        percentProgress: '0',
-        currentChapter: 'Introduction',
-        chapterTitle: '',
-      };
-    });
-    dispatch(loadBooks(savedBooks));
-  } catch (error) {
-    dispatch(noBooks(error));
   }
-}
+);
+
+export const addBook = (appId, book) => (
+  async function addBook(dispatch) {
+    const response = await postBook(appId, book);
+    if (response) {
+      dispatch(createBook(book));
+    }
+  }
+);
