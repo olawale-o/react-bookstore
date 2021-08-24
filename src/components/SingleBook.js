63096b041fd93259324ef4b89f75af517fe1b868
olawale-o <omoogunolawale@gmail.com>
2021-08-24 19:18:03 +0100

    Add screenshot.png file

--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -5,14 +5,15 @@ import BookButtons from './BookButtons';
 
 const SingleBook = ({ book }) => (
   <div className="Book">
-    <div className="book-summary">
-      <span className="book-category">{book.category}</span>
-      <div className="middle">
-        <div className="left">
-          <h4 className="book-title">{book.title}</h4>
-          <span className="book-author">{book.author}</span>
-        </div>
-        <div className="right">
+    <div className="book-panel">
+      <div className="book-panel__summary">
+        <span className="book-category">{book.category}</span>
+        <h4 className="book-title">{book.title}</h4>
+        <span className="book-author">{book.author}</span>
+        <BookButtons id={book.item_id} />
+      </div>
+      <div className="book-stats">
+        <div className="book-stats__progress">
           <div className="book-progress">
             <div className="book-progress__indicator"> </div>
             <div className="book-progress__level">
@@ -23,18 +24,18 @@ const SingleBook = ({ book }) => (
               <div className="book-progress__level-status">Completed</div>
             </div>
           </div>
+          <div className="divider"> </div>
+        </div>
+        <div className="book-chapter">
+          <p className="book-chapter__current">CURRENT CHAPTER</p>
+          <p className="book-chapter__number">
+            {book.currentChapter}
+            {book.currentChapter && book.chapterTitle && ':'}
+            {book.chapterTitle}
+          </p>
+          <button type="button" className="btn btn-progress">UPDATE PROGRESS</button>
         </div>
       </div>
-      <BookButtons id={book.item_id} />
-    </div>
-    <div className="book-chapter">
-      <p className="book-chapter__current">CURRENT CHAPTER</p>
-      <p className="book-chapter__number">
-        {book.currentChapter}
-        {book.currentChapter && book.chapterTitle && ':'}
-        {book.chapterTitle}
-      </p>
-      <button type="button" className="btn btn-progress">UPDATE PROGRESS</button>
     </div>
   </div>
 );
