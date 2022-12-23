4d313f269a08275096e16072591b54e38b52891e
olawale-o <omoogunolawale@gmail.com>
2022-12-23 18:14:33 +0100

    Create reducer and slicde

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,46 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
 import booksArray from '../../data';
-
-export const CREATE_BOOK = 'bookStore/books/CREATEBOOK';
-export const REMOVE_BOOK = 'bookStore/books/REMOVEBOOK';
-export const LOAD_BOOKS = 'bookStore/books/LOADBOOKS';
-export const NO_BOOKS = 'bookStore/books/NOBOOKS';
+import { getAllBooks, deleteSingleBook, addBook } from './books_async_actions';
 
 const initialState = booksArray;
 
-export const createBook = (payload) => ({
-  type: CREATE_BOOK,
-  payload,
-});
-
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
-
-export const loadBooks = (payload) => ({
-  type: LOAD_BOOKS,
-  payload,
-});
-
-export const noBooks = () => ({
-  type: NO_BOOKS,
-});
-
-const booksReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case CREATE_BOOK:
-      return [...state, action.payload];
-    case REMOVE_BOOK: {
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    loadBooks: (state) => state,
+    removeBook: (state, action) => {
       const filteredBooks = state.filter((book) => action.payload !== book.item_id);
       return filteredBooks;
-    }
-    case LOAD_BOOKS:
-      return state.concat(action.payload);
-    case NO_BOOKS:
-      return state;
-    default:
-      return state;
-  }
-};
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getAllBooks.fulfilled, (state, action) => {
+      const newBooks = [...state, ...action.payload];
+      return newBooks;
+    }).addCase(getAllBooks.rejected, (state) => state)
+      .addCase(deleteSingleBook.fulfilled, (state, action) => {
+        if (action.payload != null) {
+          const filteredBooks = state.filter((book) => action.payload !== book.item_id);
+          return filteredBooks;
+        }
+        return state;
+      }).addCase(addBook.fulfilled, (state, action) => {
+        state.push(action.payload);
+      });
+  },
+});
 
-export default booksReducer;
+export const { loadBooks, removeBook } = booksSlice.actions;
+
+export default booksSlice.reducer;
